93769dbe31c6d83288a9fa6724c97f1b034941b5
EinsVision <einsvision@gmail.com>
2021-02-22 02:35:55 +0900

    let's go youtube clone coding

--- youtube-clone/src/App.js
+++ youtube-clone/src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h2>Youtube clone</h2>
+      <h2>Youtube clone🎈✨🚀⭐</h2>
     </div>
   );
 }
