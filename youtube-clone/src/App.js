bdbff70ba37feb11c0dd1ccee75b7c956d55b9bd
EinsVision <einsvision@gmail.com>
2021-02-23 01:32:22 +0900

    applied React-router-dom

--- youtube-clone/src/App.js
+++ youtube-clone/src/App.js
@@ -2,15 +2,36 @@ import './App.css';
 import Header from './Header';
 import RecommendedVideos from './RecommendedVideos';
 import Sidebar from './Sidebar';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <Header />
-      <div className='app__page'>
-        <Sidebar />
-        <RecommendedVideos />
-      </div>
+      <Router>
+        <Switch>
+          
+          <Route path='/search'>
+            <h1>Search Page</h1>
+          </Route>
+
+          <Route path='/'>
+            <Header />
+            <div className='app__page'>
+              <Sidebar />
+              <RecommendedVideos />
+            </div>
+          </Route>
+
+        </Switch>
+      </Router>
+
+      
+
     </div>
   );
 }
