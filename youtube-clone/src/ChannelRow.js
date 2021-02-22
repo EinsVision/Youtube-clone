af316dc38d81e9a2634e7d7ead64b0bfbc594c77
EinsVision <einsvision@gmail.com>
2021-02-23 02:10:05 +0900

    made ChannelRow component

---
+++ youtube-clone/src/ChannelRow.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ChannelRow';
+
+function ChannelRow() {
+  return (
+    <div className='channelRow'>
+      
+    </div>
+  )
+}
+
+export default ChannelRow
--- youtube-clone/src/SearchPage.js
+++ youtube-clone/src/SearchPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './SearchPage.css';
 import TuneIcon from '@material-ui/icons/Tune';
+import ChannelRow from './ChannelRow';
 
 function SearchPage() {
   return (
@@ -10,8 +11,16 @@ function SearchPage() {
         <h2>Filter</h2>
       </div>
       <hr />
+
+      <ChannelRow
+        image='https://i.ytimg.com/vi/Sp3dFF-Bts0/maxresdefault.jpg'
+        channel='Hodny coding'
+        vertified
+        subs='659'
+        noOfVideos={382}
+        description='you can find awesome coding channel.'
+      />
     </div>
-   
   )
 }
 
