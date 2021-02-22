3fe9b5faf8d097a280e43aae3acec9e7f9a14e47
EinsVision <einsvision@gmail.com>
2021-02-22 15:01:37 +0900

    material ui installed

--- youtube-clone/src/App.js
+++ youtube-clone/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="app">
       <h2>Youtube clone🎈✨🚀⭐</h2>
+
+      <Header />
+      {/* Sidebar */}
+      {/* RecommendedVideo */}
     </div>
   );
 }
