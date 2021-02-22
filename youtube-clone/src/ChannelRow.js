c8c4d46921b7aed41260a00c5b0d69046d994a75
EinsVision <einsvision@gmail.com>
2021-02-23 02:21:57 +0900

    I'm in building the search page

--- youtube-clone/src/ChannelRow.js
+++ youtube-clone/src/ChannelRow.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './ChannelRow';
+import Avatar from '@material-ui/core/Avatar';
 
-function ChannelRow() {
+function ChannelRow({image, channel, subs, noOfVideos, description, vertified}) {
   return (
     <div className='channelRow'>
-      
+      <Avatar 
+        className='channelRow__logo'
+        alt={channel}
+        src={image}
+      />
+      <div className='channelRow__text'>
+        <h4>{channel}</h4>
+      </div>
     </div>
   )
 }
