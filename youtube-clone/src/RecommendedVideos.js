eb34a659e22728413af344872daf9694c650e95d
EinsVision <einsvision@gmail.com>
2021-02-23 01:23:13 +0900

    Recommended Video built

--- youtube-clone/src/RecommendedVideos.js
+++ youtube-clone/src/RecommendedVideos.js
@@ -6,25 +6,208 @@ function RecommendedVideos() {
   return (
     <div className='recommendedVideos'>
       <h2>Recommended</h2>
-      <div className='recommendedVideos__videos'></div>
-      <VideoCard 
-        image='https://i.ytimg.com/an_webp/l_MgB4D9-gA/mqdefault_6s.webp?du=3000&sqp=CPDRzoEG&rs=AOn4CLAknCtqvuu_31UuBsVXAthQvNnW_Q'
-        title='Son vs Salah • Who is The Deadliest Winger in EPL ?'
-        channel='Tottenham Hotspur'
-        views='2.3M Views'
-        timestamp='3 days ago'
-        channelImage='https://yt3.ggpht.com/ytc/AAUvwniWmCvWqErL14F1kNa7bgF_xoLOoeISQJ_4IXzfMsU=s88-c-k-c0x00ffffff-no-rj'
-        channelImage='https://yt3.ggpht.com/ytc/AAUvwniWmCvWqErL14F1kNa7bgF_xoLOoeISQJ_4IXzfMsU=s88-c-k-c0x00ffffff-no-rj'
-      />
-      <VideoCard />
-      <VideoCard />
-      <VideoCard />
-      <VideoCard />
-      <VideoCard />
-      <VideoCard />
-      <VideoCard />
-      <VideoCard />
-      <VideoCard />
+      <div className='recommendedVideos__videos'>
+        <VideoCard 
+          image='https://ext.fmkorea.com/files/attach/new/20180516/486616/771693052/1060057828/d7bb29b3cdd0d964583981c1c5540f82.png'
+          title='Son vs Salah • Who is The Deadliest Winger in EPL ?'
+          channel='Tottenham Hotspur'
+          views='2.3M Views'
+          timestamp='3 days ago'
+          channelImage='https://yt3.ggpht.com/ytc/AAUvwniWmCvWqErL14F1kNa7bgF_xoLOoeISQJ_4IXzfMsU=s88-c-k-c0x00ffffff-no-rj'
+        />
+        <VideoCard 
+          image='https://cdn.pixabay.com/photo/2014/09/23/06/04/brothers-457237__480.jpg'
+          title='Baby Siblings Playing and Laughing Together'
+          channel='Bright Side Korea'
+          views='1.2M Views'
+          timestamp='2 days ago'
+          channelImage='https://yt3.ggpht.com/ytc/AAUvwnhEfw23LDEIdB5PGRXLEWx6AOI919h2iA-n6uQc=s88-c-k-c0x00ffffff-no-rj'
+        />
+        <VideoCard 
+          image='https://cdn.pixabay.com/photo/2019/07/12/18/22/lamp-4333322_960_720.jpg'
+          title='Inside a $15,800,000 Modern House in West Vancouver Canada! | Propertygrams Mansion Tour'
+          channel='Angihyo'
+          views='6.3M Views'
+          timestamp='1 days ago'
+          channelImage='https://yt3.ggpht.com/ytc/AAUvwnh8nxvkNdEIBokJoyrYHLNZW1yBIbvoASHHMCu_1g=s88-c-k-c0x00ffffff-no-rj'
+        />
+        <VideoCard 
+          image='https://i.ytimg.com/vi/fF_dVhpVKaA/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBewXOplViYqzENSEgrFY0qdXL5iA'
+          title='CCM for workers'
+          channel='AnointingMusic'
+          views='5.4M Views'
+          timestamp='3 days ago'
+          channelImage='https://yt3.ggpht.com/ytc/AAUvwnhzO6dTaSIHRgB31vYLAkX-Ghf84lzSGmd2SqaNlA=s176-c-k-c0x00ffffff-no-rj-mo'
+        />
+        <VideoCard 
+          image='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQG9LqL46fc_zaigMI-yc0gbtPBL0I_ees0ug&usqp=CAU'
+          title='Unboxing new accessories for my ipad pro ✨🌨 customize with me'
+          channel='iPad nice'
+          views='2.4M Views'
+          timestamp='7 days ago'
+          channelImage='https://yt3.ggpht.com/ytc/AAUvwnjhR7lp48FSRe4LLlKW_9XioBs5HwOaj5hjNxfq2Q=s48-c-k-c0x00ffffff-no-rj'
+        />
+        <VideoCard 
+          image='https://ext.fmkorea.com/files/attach/new/20180516/486616/771693052/1060057828/d7bb29b3cdd0d964583981c1c5540f82.png'
+          title='Son vs Salah • Who is The Deadliest Winger in EPL ?'
+          channel='Tottenham Hotspur'
+          views='2.3M Views'
+          timestamp='3 days ago'
+          channelImage='https://yt3.ggpht.com/ytc/AAUvwniWmCvWqErL14F1kNa7bgF_xoLOoeISQJ_4IXzfMsU=s88-c-k-c0x00ffffff-no-rj'
+        />
+        <VideoCard 
+          image='https://cdn.pixabay.com/photo/2014/09/23/06/04/brothers-457237__480.jpg'
+          title='Baby Siblings Playing and Laughing Together'
+          channel='Bright Side Korea'
+          views='1.2M Views'
+          timestamp='2 days ago'
+          channelImage='https://yt3.ggpht.com/ytc/AAUvwnhEfw23LDEIdB5PGRXLEWx6AOI919h2iA-n6uQc=s88-c-k-c0x00ffffff-no-rj'
+        />
+        <VideoCard 
+          image='https://cdn.pixabay.com/photo/2019/07/12/18/22/lamp-4333322_960_720.jpg'
+          title='Inside a $15,800,000 Modern House in West Vancouver Canada! | Propertygrams Mansion Tour'
+          channel='Angihyo'
+          views='6.3M Views'
+          timestamp='1 days ago'
+          channelImage='https://yt3.ggpht.com/ytc/AAUvwnh8nxvkNdEIBokJoyrYHLNZW1yBIbvoASHHMCu_1g=s88-c-k-c0x00ffffff-no-rj'
+        />
+        <VideoCard 
+          image='https://i.ytimg.com/vi/fF_dVhpVKaA/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBewXOplViYqzENSEgrFY0qdXL5iA'
+          title='CCM for workers'
+          channel='AnointingMusic'
+          views='5.4M Views'
+          timestamp='3 days ago'
+          channelImage='https://yt3.ggpht.com/ytc/AAUvwnhzO6dTaSIHRgB31vYLAkX-Ghf84lzSGmd2SqaNlA=s176-c-k-c0x00ffffff-no-rj-mo'
+        />
+        <VideoCard 
+          image='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQG9LqL46fc_zaigMI-yc0gbtPBL0I_ees0ug&usqp=CAU'
+          title='Unboxing new accessories for my ipad pro ✨🌨 customize with me'
+          channel='iPad nice'
+          views='2.4M Views'
+          timestamp='7 days ago'
+          channelImage='https://yt3.ggpht.com/ytc/AAUvwnjhR7lp48FSRe4LLlKW_9XioBs5HwOaj5hjNxfq2Q=s48-c-k-c0x00ffffff-no-rj'
+        />
+        <VideoCard 
+          image='https://ext.fmkorea.com/files/attach/new/20180516/486616/771693052/1060057828/d7bb29b3cdd0d964583981c1c5540f82.png'
+          title='Son vs Salah • Who is The Deadliest Winger in EPL ?'
+          channel='Tottenham Hotspur'
+          views='2.3M Views'
+          timestamp='3 days ago'
+          channelImage='https://yt3.ggpht.com/ytc/AAUvwniWmCvWqErL14F1kNa7bgF_xoLOoeISQJ_4IXzfMsU=s88-c-k-c0x00ffffff-no-rj'
+        />
+        <VideoCard 
+          image='https://cdn.pixabay.com/photo/2014/09/23/06/04/brothers-457237__480.jpg'
+          title='Baby Siblings Playing and Laughing Together'
+          channel='Bright Side Korea'
+          views='1.2M Views'
+          timestamp='2 days ago'
+          channelImage='https://yt3.ggpht.com/ytc/AAUvwnhEfw23LDEIdB5PGRXLEWx6AOI919h2iA-n6uQc=s88-c-k-c0x00ffffff-no-rj'
+        />
+        <VideoCard 
+          image='https://cdn.pixabay.com/photo/2019/07/12/18/22/lamp-4333322_960_720.jpg'
+          title='Inside a $15,800,000 Modern House in West Vancouver Canada! | Propertygrams Mansion Tour'
+          channel='Angihyo'
+          views='6.3M Views'
+          timestamp='1 days ago'
+          channelImage='https://yt3.ggpht.com/ytc/AAUvwnh8nxvkNdEIBokJoyrYHLNZW1yBIbvoASHHMCu_1g=s88-c-k-c0x00ffffff-no-rj'
+        />
+        <VideoCard 
+          image='https://i.ytimg.com/vi/fF_dVhpVKaA/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBewXOplViYqzENSEgrFY0qdXL5iA'
+          title='CCM for workers'
+          channel='AnointingMusic'
+          views='5.4M Views'
+          timestamp='3 days ago'
+          channelImage='https://yt3.ggpht.com/ytc/AAUvwnhzO6dTaSIHRgB31vYLAkX-Ghf84lzSGmd2SqaNlA=s176-c-k-c0x00ffffff-no-rj-mo'
+        />
+        <VideoCard 
+          image='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQG9LqL46fc_zaigMI-yc0gbtPBL0I_ees0ug&usqp=CAU'
+          title='Unboxing new accessories for my ipad pro ✨🌨 customize with me'
+          channel='iPad nice'
+          views='2.4M Views'
+          timestamp='7 days ago'
+          channelImage='https://yt3.ggpht.com/ytc/AAUvwnjhR7lp48FSRe4LLlKW_9XioBs5HwOaj5hjNxfq2Q=s48-c-k-c0x00ffffff-no-rj'
+        />
+        <VideoCard 
+          image='https://ext.fmkorea.com/files/attach/new/20180516/486616/771693052/1060057828/d7bb29b3cdd0d964583981c1c5540f82.png'
+          title='Son vs Salah • Who is The Deadliest Winger in EPL ?'
+          channel='Tottenham Hotspur'
+          views='2.3M Views'
+          timestamp='3 days ago'
+          channelImage='https://yt3.ggpht.com/ytc/AAUvwniWmCvWqErL14F1kNa7bgF_xoLOoeISQJ_4IXzfMsU=s88-c-k-c0x00ffffff-no-rj'
+        />
+        <VideoCard 
+          image='https://cdn.pixabay.com/photo/2014/09/23/06/04/brothers-457237__480.jpg'
+          title='Baby Siblings Playing and Laughing Together'
+          channel='Bright Side Korea'
+          views='1.2M Views'
+          timestamp='2 days ago'
+          channelImage='https://yt3.ggpht.com/ytc/AAUvwnhEfw23LDEIdB5PGRXLEWx6AOI919h2iA-n6uQc=s88-c-k-c0x00ffffff-no-rj'
+        />
+        <VideoCard 
+          image='https://cdn.pixabay.com/photo/2019/07/12/18/22/lamp-4333322_960_720.jpg'
+          title='Inside a $15,800,000 Modern House in West Vancouver Canada! | Propertygrams Mansion Tour'
+          channel='Angihyo'
+          views='6.3M Views'
+          timestamp='1 days ago'
+          channelImage='https://yt3.ggpht.com/ytc/AAUvwnh8nxvkNdEIBokJoyrYHLNZW1yBIbvoASHHMCu_1g=s88-c-k-c0x00ffffff-no-rj'
+        />
+        <VideoCard 
+          image='https://i.ytimg.com/vi/fF_dVhpVKaA/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBewXOplViYqzENSEgrFY0qdXL5iA'
+          title='CCM for workers'
+          channel='AnointingMusic'
+          views='5.4M Views'
+          timestamp='3 days ago'
+          channelImage='https://yt3.ggpht.com/ytc/AAUvwnhzO6dTaSIHRgB31vYLAkX-Ghf84lzSGmd2SqaNlA=s176-c-k-c0x00ffffff-no-rj-mo'
+        />
+        <VideoCard 
+          image='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQG9LqL46fc_zaigMI-yc0gbtPBL0I_ees0ug&usqp=CAU'
+          title='Unboxing new accessories for my ipad pro ✨🌨 customize with me'
+          channel='iPad nice'
+          views='2.4M Views'
+          timestamp='7 days ago'
+          channelImage='https://yt3.ggpht.com/ytc/AAUvwnjhR7lp48FSRe4LLlKW_9XioBs5HwOaj5hjNxfq2Q=s48-c-k-c0x00ffffff-no-rj'
+        />
+        <VideoCard 
+          image='https://ext.fmkorea.com/files/attach/new/20180516/486616/771693052/1060057828/d7bb29b3cdd0d964583981c1c5540f82.png'
+          title='Son vs Salah • Who is The Deadliest Winger in EPL ?'
+          channel='Tottenham Hotspur'
+          views='2.3M Views'
+          timestamp='3 days ago'
+          channelImage='https://yt3.ggpht.com/ytc/AAUvwniWmCvWqErL14F1kNa7bgF_xoLOoeISQJ_4IXzfMsU=s88-c-k-c0x00ffffff-no-rj'
+        />
+        <VideoCard 
+          image='https://cdn.pixabay.com/photo/2014/09/23/06/04/brothers-457237__480.jpg'
+          title='Baby Siblings Playing and Laughing Together'
+          channel='Bright Side Korea'
+          views='1.2M Views'
+          timestamp='2 days ago'
+          channelImage='https://yt3.ggpht.com/ytc/AAUvwnhEfw23LDEIdB5PGRXLEWx6AOI919h2iA-n6uQc=s88-c-k-c0x00ffffff-no-rj'
+        />
+        <VideoCard 
+          image='https://cdn.pixabay.com/photo/2019/07/12/18/22/lamp-4333322_960_720.jpg'
+          title='Inside a $15,800,000 Modern House in West Vancouver Canada! | Propertygrams Mansion Tour'
+          channel='Angihyo'
+          views='6.3M Views'
+          timestamp='1 days ago'
+          channelImage='https://yt3.ggpht.com/ytc/AAUvwnh8nxvkNdEIBokJoyrYHLNZW1yBIbvoASHHMCu_1g=s88-c-k-c0x00ffffff-no-rj'
+        />
+        <VideoCard 
+          image='https://i.ytimg.com/vi/fF_dVhpVKaA/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBewXOplViYqzENSEgrFY0qdXL5iA'
+          title='CCM for workers'
+          channel='AnointingMusic'
+          views='5.4M Views'
+          timestamp='3 days ago'
+          channelImage='https://yt3.ggpht.com/ytc/AAUvwnhzO6dTaSIHRgB31vYLAkX-Ghf84lzSGmd2SqaNlA=s176-c-k-c0x00ffffff-no-rj-mo'
+        />
+        <VideoCard 
+          image='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQG9LqL46fc_zaigMI-yc0gbtPBL0I_ees0ug&usqp=CAU'
+          title='Unboxing new accessories for my ipad pro ✨🌨 customize with me'
+          channel='iPad nice'
+          views='2.4M Views'
+          timestamp='7 days ago'
+          channelImage='https://yt3.ggpht.com/ytc/AAUvwnjhR7lp48FSRe4LLlKW_9XioBs5HwOaj5hjNxfq2Q=s48-c-k-c0x00ffffff-no-rj'
+        />
+      </div>
     </div>
   )
 }
